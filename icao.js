587781d1a32998af0d8d5736b793725f6d374eae
jansec15 <101845158+jansec15@users.noreply.github.com>
2022-12-21 15:23:59 -0500

    Update icao.js

--- icao.js
+++ icao.js
@@ -21,7 +21,6 @@ async function icao(from, to) {
     const stats = await PCR(option);
     process.env.PUPPETEER_EXECUTABLE_PATH = stats.executablePath;
     const browser = await puppeteer.launch({
-        headers: { "Accept-Encoding": "gzip,deflate,compress" },
         headless: true,
         args: ["--no-sandbox"],
         executablePath: stats.executablePath,
