65cee9f35a55940309b0625114f03310837bc012
jansec15 <101845158+jansec15@users.noreply.github.com>
2024-05-30 15:11:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,98 +6,98 @@ const PORT = process.env.PORT || 3030;
 
 'use strict';
 const fs = require('fs');
-const mysql = require('mysql')
+// const mysql = require('mysql')
 const rawdata = fs.readFileSync('country.json');
 const countries = JSON.parse(rawdata);
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'icao_cache'
-})
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'icao_cache'
+// })
 // console.log(countries);
 // app.get('/:from&:to&:type',  (request, response) => {
 //     return response.send("404").end()
 // })
 // --CRUD--
 //LIST
-function list_cache() {
-    return pool.getConnection((err, connection) => {
-                if (err) throw err
-                console.log('connected as id ' + connection.threadId)
-                connection.query('SELECT * from flight', (err, rows) => {
-                    connection.release() // return the connection to pool
+// function list_cache() {
+//     return pool.getConnection((err, connection) => {
+//                 if (err) throw err
+//                 console.log('connected as id ' + connection.threadId)
+//                 connection.query('SELECT * from flight', (err, rows) => {
+//                     connection.release() // return the connection to pool
 
-                    if (!err) {
-                        return rows
-                    } else {
-                        console.log(err)
-                        return []
-                    }
+//                     if (!err) {
+//                         return rows
+//                     } else {
+//                         console.log(err)
+//                         return []
+//                     }
 
-                    // if(err) throw err
-                    console.log('The icao_cache from beer table are: \n', rows)
-                })
-            })
-}
-//ADD
-function add_cache(data){
-    pool.getConnection((err, connection) => {
-        if (err) throw err
+//                     // if(err) throw err
+//                     console.log('The icao_cache from beer table are: \n', rows)
+//                 })
+//             })
+// }
+// //ADD
+// function add_cache(data){
+//     pool.getConnection((err, connection) => {
+//         if (err) throw err
 
-        const params = data
-        connection.query('INSERT INTO icao_cache SET ?', params, (err, rows) => {
-            connection.release() // return the connection to pool
-            if (!err) {
-                console.log(`Cache with the record ID  has been added.`)
-            } else {
-                console.log(err)
-            }
+//         const params = data
+//         connection.query('INSERT INTO icao_cache SET ?', params, (err, rows) => {
+//             connection.release() // return the connection to pool
+//             if (!err) {
+//                 console.log(`Cache with the record ID  has been added.`)
+//             } else {
+//                 console.log(err)
+//             }
 
-            console.log('The data from beer table are:11 \n', rows)
+//             console.log('The data from beer table are:11 \n', rows)
 
-        })
-    })
-}
-//UPDATE
-function update_cache(data){
-    pool.getConnection((err, connection) => {
-        if (err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+//         })
+//     })
+// }
+// //UPDATE
+// function update_cache(data){
+//     pool.getConnection((err, connection) => {
+//         if (err) throw err
+//         console.log(`connected as id ${connection.threadId}`)
 
-        const { id, time_stamp, origin, destination, main, detail } = data
+//         const { id, time_stamp, origin, destination, main, detail } = data
 
-        connection.query('UPDATE icao_cache SET time_stamp = ?, origin = ?, destination = ?, main = ?, detail = ? WHERE id = ?', [time_stamp, origin, destination, main, detail, id], (err, rows) => {
-            connection.release() // return the connection to pool
+//         connection.query('UPDATE icao_cache SET time_stamp = ?, origin = ?, destination = ?, main = ?, detail = ? WHERE id = ?', [time_stamp, origin, destination, main, detail, id], (err, rows) => {
+//             connection.release() // return the connection to pool
 
-            if (!err) {
-                console.log(`Flight with the rute: ${origin} - ${destination} has been update.`)
-            } else {
-                console.log(err)
-            }
+//             if (!err) {
+//                 console.log(`Flight with the rute: ${origin} - ${destination} has been update.`)
+//             } else {
+//                 console.log(err)
+//             }
 
-        })
+//         })
 
-        console.log(data)
-    })
-}
-//DELETE
-function delete_cache(id){
-    pool.getConnection((err, connection) => {
-        if (err) throw err
-        connection.query('DELETE FROM beers WHERE id = ?', [id], (err, rows) => {
-            connection.release() // return the connection to pool
-            if (!err) {
-                console.log(`icao_cache with the record ID ${[id]} has been removed.`)
-            } else {
-                console.log(err)
-            }
+//         console.log(data)
+//     })
+// }
+// //DELETE
+// function delete_cache(id){
+//     pool.getConnection((err, connection) => {
+//         if (err) throw err
+//         connection.query('DELETE FROM beers WHERE id = ?', [id], (err, rows) => {
+//             connection.release() // return the connection to pool
+//             if (!err) {
+//                 console.log(`icao_cache with the record ID ${[id]} has been removed.`)
+//             } else {
+//                 console.log(err)
+//             }
 
-            console.log('The data from icao_cache table are: \n', rows)
-        })
-    })
-}
+//             console.log('The data from icao_cache table are: \n', rows)
+//         })
+//     })
+// }
 function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, (s * 1000)));
 }
