f3be92c394dc2aacb4171a5a83d7917f6cb6a18d
jansec15 <juansecuan3@hotmail.com>
2025-12-29 09:59:20 -0500

    Update icao.js

--- icao.js
+++ icao.js
@@ -32,6 +32,7 @@ async function loadChromium() {
     }
 }
 async function icao(from, to) {
+
     const executablePath = await loadChromium();
     if (!executablePath) {
         console.error("No se pudo iniciar Puppeteer porque no se encontró Chromium.");
@@ -52,127 +53,133 @@ async function icao(from, to) {
         console.log(error);
     });
     // const browser = await puppeteer.launch({ headless: false });
-
-    // inicia el navegador
-    const page = await browser.newPage();
-
-    //funcion para enviar la variable from al navegador
-    await page.exposeFunction("getFrom", function () {
-        return from;
-    });
-    //funcion para enviar la variable to al navegador
-    await page.exposeFunction("getTo", function () {
-        return to;
-    });
-    //permite la cargar correctamente la pagina
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
-
-    //crea una pestaña con la url
-    await page.goto(baseurl);
-
-    await page.waitForSelector('[aria-labelledby="select2-SelectPassengerDestination1-container"]');
-    //ejecuta codigo en el navegador
-    await page.evaluate(async () => {
-        var opt1 = "#SelectPassengerDeparture";
-        var opt2 = "#SelectPassengerDestination1";
-        //cargar variable from con la funcion getFrom()
-        var frm = await getFrom();
-        console.log("From: " + frm);
-        //input de la pagina para origen de vuelo
-        $(opt1).val(frm);
-        // $(opt2).empty();
-        //funcion de la pagina icao que actualiza el input de destino
-        // Reduce("second");
-        PassengerGetAirportsByDeparture(frm, false, "Departure");
-        const delay = ms => new Promise(res => setTimeout(res, ms));
-        await delay(500);
-        //cargar variable to con la funcion getTo()
-        var to = await getTo();
-        //actualiza inputs de la pagina
-        $(opt2).val(to);
-        PassengerGetAirportsByDeparture(to, false, "Destination1");
-        // Reduce("second");
-        // $(opt + "frm3").val(frm);
-    }).catch(error => {
-        console.log(error);
-        return undefined;
-    });
-
-    //envia el formulario
     try {
-        await Promise.race([
-            page.click('#calculateButton'),
-            new Promise((_, reject) => {
-                setTimeout(() => {
-                    console.log("Límite de tiempo excedido")
-                    reject(new Error('Límite de tiempo excedido'));
-                }, 3000); // Tiempo límite en milisegundos (en este caso, 5 segundos)
-            })
-        ]);
-    } catch (error) {
-        console.log(error);
-        await browser.close();
-        return undefined;
-    }
+        // inicia el navegador
+        const page = await browser.newPage();
 
-    //div que contiene los resultados
-    await page.waitForSelector('div#ResultDivPassengerEconomyMetric .body-content-result');
+        //funcion para enviar la variable from al navegador
+        await page.exposeFunction("getFrom", function () {
+            return from;
+        });
+        //funcion para enviar la variable to al navegador
+        await page.exposeFunction("getTo", function () {
+            return to;
+        });
+        //permite la cargar correctamente la pagina
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
 
-    //codigo para extraer respuesta del navegador
-    const result = await page.evaluate(async () => {
-        var table = document.querySelectorAll('div#ResultDivPassengerEconomyMetric .body-content-result');
-        var result = [];
+        //crea una pestaña con la url
+        await page.goto(baseurl);
 
-        for (var i = 0; i < table.length; i++) {
-            let elementLabel = table[i].querySelectorAll(".result-item-label-layout label")
-            let elementValue = table[i].querySelectorAll(".result-item-value-layout label")
-            let fuel = "";
-            if (elementLabel && elementLabel[1].innerHTML == "Aircraft Fuel Burn/leg") {
-                console.log(elementLabel[1].innerHTML)
-                console.log(elementValue[1].innerHTML)
-                fuel = elementValue[1].innerHTML;
-                result.push(["Aircraft Fuel Burn/leg", fuel]);
-                continue
-            }
-            // let element = table[i].querySelectorAll('label');
-            let key = elementLabel[2].innerHTML
-            let value = elementValue[2].innerHTML;
-            if (key == "" || value == "") {
-                continue
-            }
-            result.push([key, value]);
+        await page.waitForSelector('[aria-labelledby="select2-SelectPassengerDestination1-container"]');
+        //ejecuta codigo en el navegador
+        await page.evaluate(async () => {
+            var opt1 = "#SelectPassengerDeparture";
+            var opt2 = "#SelectPassengerDestination1";
+            //cargar variable from con la funcion getFrom()
+            var frm = await getFrom();
+            console.log("From: " + frm);
+            //input de la pagina para origen de vuelo
+            $(opt1).val(frm);
+            // $(opt2).empty();
+            //funcion de la pagina icao que actualiza el input de destino
+            // Reduce("second");
+            PassengerGetAirportsByDeparture(frm, false, "Departure");
+            const delay = ms => new Promise(res => setTimeout(res, ms));
+            await delay(500);
+            //cargar variable to con la funcion getTo()
+            var to = await getTo();
+            //actualiza inputs de la pagina
+            $(opt2).val(to);
+            PassengerGetAirportsByDeparture(to, false, "Destination1");
+            // Reduce("second");
+            // $(opt + "frm3").val(frm);
+        }).catch(error => {
+            console.log(error);
+            return undefined;
+        });
+
+        //envia el formulario
+        try {
+            await Promise.race([
+                page.click('#calculateButton'),
+                new Promise((_, reject) => {
+                    setTimeout(() => {
+                        console.log("Límite de tiempo excedido")
+                        reject(new Error('Límite de tiempo excedido'));
+                    }, 3000); // Tiempo límite en milisegundos (en este caso, 5 segundos)
+                })
+            ]);
+        } catch (error) {
+            console.log(error);
+            await browser.close();
+            return undefined;
         }
-        return result;
-    });
-    //cierra el navegador
-    await browser.close();
 
-    //si por alguna razon no tiene resultado
-    if (result.length == 0) {
-        return undefined;
+        //div que contiene los resultados
+        await page.waitForSelector('div#ResultDivPassengerEconomyMetric .body-content-result');
+
+        //codigo para extraer respuesta del navegador
+        const result = await page.evaluate(async () => {
+            var table = document.querySelectorAll('div#ResultDivPassengerEconomyMetric .body-content-result');
+            var result = [];
+
+            for (var i = 0; i < table.length; i++) {
+                let elementLabel = table[i].querySelectorAll(".result-item-label-layout label")
+                let elementValue = table[i].querySelectorAll(".result-item-value-layout label")
+                let fuel = "";
+                if (elementLabel && elementLabel[1].innerHTML == "Aircraft Fuel Burn/leg") {
+                    console.log(elementLabel[1].innerHTML)
+                    console.log(elementValue[1].innerHTML)
+                    fuel = elementValue[1].innerHTML;
+                    result.push(["Aircraft Fuel Burn/leg", fuel]);
+                    continue
+                }
+                // let element = table[i].querySelectorAll('label');
+                let key = elementLabel[2].innerHTML
+                let value = elementValue[2].innerHTML;
+                if (key == "" || value == "") {
+                    continue
+                }
+                result.push([key, value]);
+            }
+            return result;
+        });
+        //cierra el navegador
+        await browser.close();
+
+        //si por alguna razon no tiene resultado
+        if (result.length == 0) {
+            return undefined;
+        }
+        // let origin = 7;
+        // let destination = 12;
+        //el valor de origen y destino, trae un label que contiene Passenger si no lo contiene es que o cambio o existe problema
+        // if (!result[origin][0].includes('Passenger CO')) {
+        //     return undefined;
+        // }
+
+        // if (!result[destination][0].includes('Passenger CO')) {
+        //     return undefined;
+        // }
+        let response = {};
+        //replace(/\D+/g, "") quita todo lo que no sea numero
+        response.main = parseFloat(result[0][1].replace(/[^0-9.]+/g, ""));
+        response.detail1 = parseFloat(result[1][1].replace(/[^0-9.]+/g, ""));
+        response.detail2 = parseFloat(result[3][1].replace(/[^0-9.]+/g, ""));
+        response.fuel1 = parseFloat(result[2][1].replace(/[^0-9.]+/g, ""));
+        response.fuel2 = parseFloat(result[4][1].replace(/[^0-9.]+/g, ""));
+        const end = new Date() - start;
+        console.log(`Tiempo de ejecución ${end} ms`);
+        // console.log(result);
+        // console.log(response)
+        return response;
+    } catch (e) {
+        console.log(e)
+        return null;
+    } finally {
+        await browser.close();
     }
-    // let origin = 7;
-    // let destination = 12;
-    //el valor de origen y destino, trae un label que contiene Passenger si no lo contiene es que o cambio o existe problema
-    // if (!result[origin][0].includes('Passenger CO')) {
-    //     return undefined;
-    // }
-
-    // if (!result[destination][0].includes('Passenger CO')) {
-    //     return undefined;
-    // }
-    let response = {};
-    //replace(/\D+/g, "") quita todo lo que no sea numero
-    response.main = parseFloat(result[0][1].replace(/[^0-9.]+/g,""));
-    response.detail1 = parseFloat(result[1][1].replace(/[^0-9.]+/g,""));
-    response.detail2 = parseFloat(result[3][1].replace(/[^0-9.]+/g,""));
-    response.fuel1 = parseFloat(result[2][1].replace(/[^0-9.]+/g,""));
-    response.fuel2 = parseFloat(result[4][1].replace(/[^0-9.]+/g,""));
-    const end = new Date() - start;
-    console.log(`Tiempo de ejecución ${end} ms`);
-    // console.log(result);
-    // console.log(response)
-    return response;
 }
 
 module.exports = {
